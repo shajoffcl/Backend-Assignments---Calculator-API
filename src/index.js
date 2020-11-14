add0a21596d5d8462f7c4c0d7e27aa423ac702a5
shajoffcl <shaj.native@gmail.com>
2020-11-14 16:36:14 +0530

    changes done

--- src/index.js
+++ src/index.js
@@ -8,12 +8,37 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json())
-// your code goes here
+app.use(bodyParser.json());
 
+app.post('/add', (req,res)=>{
+    const response={
+        status: "success",
+        message: "the sum of given two numbers",
+        sum: req.body.num1+req.body.num2
+    }
+    res.send(response);
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.post('/sub', (req, res)=>{
+    const response={
+        status: "success",
+        message: "the difference of given two numbers",
+        difference: req.body.num1-req.body.num2
+    }
+    res.send(response);
+});
+
+app.post('/multiply', (req, res)=>{
+    const response={
+        status: "success",
+        message: "The product of given numbers",
+        result: req.body.num1*req.body.num2
+    }
+    res.send(response);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
