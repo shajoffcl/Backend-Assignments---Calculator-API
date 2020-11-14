034724beb98828a13e53b0ba1bc1bc6258bd29ba
shajoffcl <shaj.native@gmail.com>
2020-11-14 17:04:54 +0530

    updated 0.0

--- src/index.js
+++ src/index.js
@@ -17,28 +17,58 @@ app.get('/', (req, res)=>{
 });
 
 app.post('/add', (req,res)=>{
-    const response={
-        status: "success",
-        message: "the sum of given two numbers",
-        sum: req.body.num1+req.body.num2
+    let num1=req.body.num1;
+    let num2=req.body.num2;
+    if(typeof num1==="string" || typeof num2==="string"){
+        const result={
+            status:"error",
+            message:"Invalid data types"
+        }
+        res.send(result);
+        return;
     }
-    res.send(response);
+    const result={
+        status:"success",
+        message:"the sum of given two numbers",
+        sum:num1+num2
+    }
+    res.send(result);
 });
 
 app.post('/sub', (req, res)=>{
+    let num1=req.body.num1;
+    let num2=req.body.num2;
+    if(typeof num1==="string" || typeof num2==="string"){
+        const result={
+            status:"error",
+            message:"Invalid data types"
+        }
+        res.send(result);
+        return;
+    }
     const response={
         status: "success",
         message: "the difference of given two numbers",
-        difference: req.body.num1-req.body.num2
+        difference: num1-num2
     }
     res.send(response);
 });
 
 app.post('/multiply', (req, res)=>{
+    let num1=req.body.num1;
+    let num2=req.body.num2;
+    if(typeof num1==="string" || typeof num2==="string"){
+        const result={
+            status:"error",
+            message:"Invalid data types"
+        }
+        res.send(result);
+        return;
+    }
     const response={
         status: "success",
         message: "The product of given numbers",
-        result: req.body.num1*req.body.num2
+        result: num1*num2
     }
     res.send(response);
 });
